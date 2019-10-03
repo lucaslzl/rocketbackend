511be80c27afc90ab386f4e955802145edbfb675
Lucas Zanco Ladeira <lznladeira@gmail.com>
2019-10-02 23:40:15 -0300

    Improve usability for the frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -10,7 +12,10 @@ mongoose.connect('mongodb+srv://omnistack:omni1234@omnistack-ynoto.mongodb.net/t
     useUnifiedTopology: true,
 });
 
+//app.use(cors(origin: 'http://localhost:3000'));
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
